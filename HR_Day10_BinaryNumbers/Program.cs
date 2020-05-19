d5297302085b137735bca2afdb7ebbd6c49b5950
aarthi <aarthi.saba@gmail.com>
2020-05-19 14:21:26 -0400

    my solution

--- HR_Day10_BinaryNumbers/Program.cs
+++ HR_Day10_BinaryNumbers/Program.cs
@@ -8,53 +8,34 @@ namespace HR_Day10_BinaryNumbers
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            //string output = Convert.ToString(n, 2);
-            int count =0;
+            string output = Convert.ToString(n, 2);
+
+            int count = 1;
             int max = 0;
-            while ( n > 0)
+            if (!output.Contains('1'))
             {
-                if(n%2 ==1)
-                {
-                    count += 1;
-                    if(max < count)
-                    {
-                        max = count;
-                    }
-                }
-                else
-                {
-                    count = 0;
-                }
-                n = n / 2;
+                count = 0;
 
             }
-            Console.WriteLine(max);
-            //int count = 1;
-            //int max = 0;
-            //if (!output.Contains('1'))
-            //{
-            //    count = 0;
-
-            //}
-            //else
-            //{
-            //    for (int i = 0; i < output.Length - 1; i++)
-            //    {
-            //        if (output[i] == '1' && output[i + 1] == '1')
-            //        {
-            //            count += 1;
-            //        }
-            //        else if (output[i] == '0')
-            //        {
-            //            if (max < count)
-            //            {
-            //                max = count;
-            //            }
-            //            count = 1;
-            //        }
-            //    }
-            //}
-            //Console.WriteLine(max < count ? count :max);
+            else
+            {
+                for (int i = 0; i < output.Length - 1; i++)
+                {
+                    if (output[i] == '1' && output[i + 1] == '1')
+                    {
+                        count += 1;
+                    }
+                    else if (output[i] == '0')
+                    {
+                        if (max < count)
+                        {
+                            max = count;
+                        }
+                        count = 1;
+                    }
+                }
+            }
+            Console.WriteLine(max < count ? count : max);
         }
     }
 }
